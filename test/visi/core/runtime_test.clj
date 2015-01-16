363dd74a4f99d2ca494538e4b4802bbd2110f2cd
Xah Lee <xah@xahlee.org>
2015-01-16 03:36:24 -0800

    Added 2 dummy files runtime_test.clj, util_test.clj

---
+++ test/visi/core/runtime_test.clj
@@ -0,0 +1,9 @@
+(ns visi.core.runtime-test
+    (:require [clojure.test :as t]
+              [visi.core.parser :as vp]
+              [visi.core.runtime :as vr]
+              [visi.core.util :as vu]
+              [instaparse.core :as insta]
+              ))
+
+(pr "yes" )
